b7abf18b85936d377db0cb9e7ebc4c61e888a1d4
Pabel Hernandez <pabel.hernandez@live.com>
2023-04-17 11:14:15 -0600

    update source search partameter

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,27 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
         HttpClient client = HttpClient.newHttpClient();
         String url = "https://serpapi.com/search.json?engine=google_scholar";
-        String apiKey = "abc123"; //your apiKey Ex: 123abc
+        String apiKey = "123abc"; //your apiKey Ex: 123abc
         String query = "coffee";
         String hl = "en";
         int numResults = 10;
-        String author = "Cano";
-        String journal = "Elsevier";
+        String author = "";// Optional
+        String source = ""; //Optional
 
         // if a value is provided for author
         if (args.length > 0) {
             author = "&author=" + args[0];
         }
 
-        // if a value is provided for the journal
+        // if a value is provided for the source
         if (args.length > 1) {
-            journal = "&journal=" + args[1];
+            source = "&source=" + args[1];
         }
 
         // build the query string
         // Basic query
         // https://serpapi.com/search.json?engine=google_scholar&q=brain&hl=en&num=10&api_key=123abc
-        String requestUrl = url + "&q=" + query + "&hl=" + hl + "&num=" + numResults + author + journal + "&api_key=" + apiKey;
+        String requestUrl = url + "&q=" + query + "&hl=" + hl + "&num=" + numResults + author + source + "&api_key=" + apiKey;
 
         HttpRequest request = HttpRequest.newBuilder().uri(new URI(requestUrl)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
