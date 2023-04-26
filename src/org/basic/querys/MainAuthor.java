79bed3d4129547c8f97a9833e7b8357bb2f385d1
Pabel Hernández <pabel.hernandez@live.com>
2023-04-26 00:07:35 -0600

    update add info to MySQL

--- src/org/basic/querys/MainAuthor.java
+++ src/org/basic/querys/MainAuthor.java
@@ -1,17 +1,26 @@
 package org.basic.querys;
 
+
+import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainAuthor {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+         final String DB_URL = "jdbc:mysql://localhost:3306/authorsdata";
+         final String USER = "root";
+         final String PASS = "";
         HttpClient client = HttpClient.newHttpClient();
         String url = "https://serpapi.com/search.json?engine=google_scholar_author";
-        String apiKey = "123abc"; //your apiKey Ex: 123abc
+        String apiKey = "abc123"; //your apiKey Ex: 123abc
         String author_id = "qc6CJjYAAAAJ"; //Albert Einstein
         String hl = "en";
         int numResults = 10;
@@ -21,6 +30,39 @@ public class MainAuthor {
 
         HttpRequest request = HttpRequest.newBuilder().uri(new URI(requestUrl)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        // parse the JSON response
+
+        String json_data = response.body();
+
+        JSONObject data = new JSONObject(json_data);
+
+        String name = data.getJSONObject("author").getString("name");
+        String affiliations = data.getJSONObject("author").getString("affiliations");
+        String email = data.getJSONObject("author").getString("email");
+        String google_scholar_author_url = data.getJSONObject("search_metadata").getString("google_scholar_author_url");
+
+        System.out.println(name);
+        System.out.println(affiliations);
+        System.out.println(email);
+        System.out.println(google_scholar_author_url);
+
+
+
+            // Open a connection
+            try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                 Statement stmt = conn.createStatement()
+            ) {
+                // Execute a query
+                // authors (author_id, articles, affiliations,email)
+                System.out.println("Inserting records into the table...");
+                String sql = "INSERT INTO authors (nombre, afiliaciones, email, url) VALUES ('"+name+"', '"+affiliations+"', '"+email+"', '"+google_scholar_author_url+"')";
+                stmt.executeUpdate(sql);
+
+                System.out.println("Inserted records into the table...");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
+
